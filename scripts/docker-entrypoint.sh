d01e327c395a553ec389cdd4f28dd4b65a4d0458
Mohd Aiman Saleem <aimansaleemmohd@gmail.com>
2025-08-10 12:53:43 +0530

    fixed 404

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -180,7 +180,7 @@ http {
 
     location /api/ {
 
-      proxy_pass ${BACKEND_URL}/;
+      proxy_pass ${BACKEND_URL}/api/;
 
       proxy_set_header Host dino-backend-api-867506203789.us-central1.run.app;
 
